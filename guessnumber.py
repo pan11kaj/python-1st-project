1d0a5fd950194ea2125d39c10d9692ce718def25
pan11kaj <68064022+pan11kaj@users.noreply.github.com>
2020-12-28 17:36:52 +0530

    Add files via upload

---
+++ guessnumber.py
@@ -0,0 +1,31 @@
+import  random
+print("guess number game")
+print("guess a number (between 1-9)")
+guessnum = int(input('enter your number:-'))
+chances = 1
+number = random.randrange(1,9)
+
+while chances<5:
+    if(guessnum < number  ):
+        print('to loo please try again')
+        guessnum = int(input('enter your number:'))
+        print('to loo please try again')
+        guessnum = int(input('enter your number:'))
+        print('to loo please try again')
+        guessnum = int(input('enter your number:'))
+        print('to loo please try again')
+        guessnum = int(input('enter your number:'))
+
+        break
+    if(guessnum != number):
+        chances = chances+1 
+        break
+    if (chances<5):
+        print('you lose the game the number is')
+        chances =0
+        break
+    if(guessnum == number):
+        print('congratulations You win the game')
+        chances =0
+        break
+        
